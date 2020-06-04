0f179e601ad7d212aed05ae4db492abc6b0504fc
Peter Nguyen <npeter.dev@gmail.com>
2020-06-05 06:32:35 +1000

    Add basic login & signup pages

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,11 +1,49 @@
 import React from "react";
 import MinNav from "./../shared/min_nav";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 export default function Login() {
   return (
     <React.Fragment>
       <MinNav />
-      <h1>Login</h1>
+      <Container id="main-container">
+        <div id="login">
+          <Row className="justify-content-center">
+            <Col sm={12} md={9} lg={6}>
+              <Form>
+                <h1 className="text-center">Log in</h1>
+                <Form.Group controlId="email">
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="Email Address"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="password">
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Button variant="primary" type="submit" size="lg" block>
+                    Log in
+                  </Button>
+                </Form.Group>
+              </Form>
+              <div className="text-center">
+                <p>
+                  Not a member? <Link to="/signup">Sign up now!</Link>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </React.Fragment>
   );
 }
--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -1,11 +1,76 @@
 import React from "react";
 import MinNav from "./../shared/min_nav";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 export default function Signup() {
   return (
     <React.Fragment>
       <MinNav />
-      <h1>Signup</h1>
+      <Container id="main-container">
+        <div id="signup">
+          <Row className="justify-content-center">
+            <Col sm={12} md={9} lg={6}>
+              <Form>
+                <h1 className="text-center">Sign up</h1>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="fname">
+                    <Form.Control
+                      name="fname"
+                      placeholder="First Name"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="lname">
+                    <Form.Control name="lname" placeholder="Last Name" />
+                  </Form.Group>
+                </Form.Row>
+                <Form.Group controlId="email">
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="Email Address"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="username">
+                  <Form.Control
+                    name="username"
+                    placeholder="Username"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="password">
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="passwordConfirmation">
+                  <Form.Control
+                    type="password"
+                    name="passwordConfirmation"
+                    placeholder="Confirm Password"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Button variant="primary" type="submit" size="lg" block>
+                    Sign up
+                  </Button>
+                </Form.Group>
+              </Form>
+              <div className="text-center">
+                <p>
+                  Already have an account? <Link to="/login">Log in here!</Link>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </React.Fragment>
   );
 }
