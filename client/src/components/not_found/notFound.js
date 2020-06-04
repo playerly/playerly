edf4fe925b167f2fdd072db77b2be04cf5df2d2b
Peter Nguyen <npeter.dev@gmail.com>
2020-06-05 07:08:27 +1000

    Update not found page visuals

--- client/src/components/not_found/notFound.js
+++ client/src/components/not_found/notFound.js
@@ -1,11 +1,20 @@
 import React from "react";
+import MinNav from "./../shared/min_nav";
+import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 export default function NotFound() {
   return (
     <React.Fragment>
-      <Container>
-        <h1>404: Not found</h1>
+      <MinNav />
+      <Container id="main-container">
+        <div id="not-found">
+          <h1>404</h1>
+          <h3>Oops! Looks like this page doesn't exist.</h3>
+          <Link to="/" className="btn btn-lg btn-primary">
+            Return to Playerly
+          </Link>
+        </div>
       </Container>
     </React.Fragment>
   );
